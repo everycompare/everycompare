248bff1185b9c39812e7fc09f9ae29fc9fd66268
Michał Woźniczak <michal.wozniczak@gmail.com>
2019-06-07 17:26:05 +0200

    Added a structure for creating force graphs

--- everycompare/structures.py
+++ everycompare/structures.py
@@ -2,3 +2,5 @@ from collections import namedtuple
 
 ComparisonResult = namedtuple('ComparisonResult', ('difference', 'paths', 'method'))
 FileMeta = namedtuple('FileMeta', ('path', 'is_binary', 'contents', 'size', 'text_length'))
+
+ForceGraphNode = namedtuple('ForceGraphNode', ('id', 'group'))
